1e693b4b8e933def4f211b84261e4e4b4411cfc7
grakzanchetta <grakzanchetta@gmail.com>
2022-08-31 19:46:14 -0300

    new commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
+import router from "../src/routers/index"
 
 dotenv.config();
 
 const app = express();
 app.use(json());
+app.use(router);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- src/repositories/fighterRepository.ts
+++ src/repositories/fighterRepository.ts
@@ -1,6 +1,8 @@
 import { connection } from "../databases/database";
 
 export async function find(){
-    
-    
+    const result = await connection.query(
+        `SELECT * FROM fighters`
+    )
+    return (result.rows)
 }
